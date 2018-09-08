5e51989e2700fcbe980ccb37a14b0fdf74882bb8
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-09-08 12:04:15 +0100

    Express server for static content and sensor api requests.

--- server.js
+++ server.js
@@ -1,13 +1,58 @@
-let serveStatic = require('serve-static');
-let http = require('http');
+// server.js
+// where your node app starts
 
-// Serve up the public folder
-let serve = serveStatic('public', {'index': ['index.html', 'index.htm']})
+// init project
+let express = require('express');
+let app = express();
 
-// Create server
-let server = http.createServer(function onRequest (req, res) {
-    serve(req, res, undefined)
-})
+const sensorTypes = require('./lib/get-sensor-types.js')
+const oracledb = require('oracledb')
+const dbconfig = require('./lib/db-config.js')
 
-// Listen
-server.listen(3000)
+async function createDbConnectionPool() {
+    try {
+        await oracledb.createPool(dbconfig)
+        console.log("Default DB connection pool created")
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+createDbConnectionPool()
+
+async function getSensorTypes() { 
+    return new Promise(async function(resolve, reject) {
+        try {
+            let types = await sensorTypes.getSensorTypes()
+            console.log(JSON.stringify(types))
+            
+            resolve(JSON.stringify(types))
+        }
+        catch(error) {
+            console.log(error)
+            reject(error)
+        }
+    })
+}
+
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/api/sensor-types", function (request, response) { 
+    getSensorTypes()
+        .then((results, error) => {
+            if(error) {
+                response.writeHead(500, {'Content-Type': 'application/json'});    
+            }
+            else {
+                response.writeHead(200, {'Content-Type': 'application/json'});
+                response.end(results)
+            }
+        })
+});
+
+// listen for requests :)
+let listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
