6af3f4911ed8eb8fb12a7b54a0e6f266ff15b494
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-09-14 19:53:03 +0100

    Minor tweaks

--- public/client.js
+++ public/client.js
@@ -1,17 +1,15 @@
 
-
 (function() {
     // load the options for the sensor type select
     fetch('/api/sensor-types').then((response) => {
         return response.json()
     }).then((json) => {
-            console.log(json)
-            json.rows.forEach(element => {
+            let selectElement = document.getElementById("sensorTypeSelect")
+            json.rows.forEach(sensorType => {
                 let newOption = document.createElement("option")
-                newOption.setAttribute("value", `${element[0]}   ${element[1]}`)
+                newOption.setAttribute("value", `${sensorType[0]}   ${sensorType[1]}`)
                 newOption.innerHTML = newOption.value
-                let selectElement = document.getElementById("sensorTypeSelect")
                 selectElement.appendChild(newOption)
-            });         
+            })        
         })
 })();
