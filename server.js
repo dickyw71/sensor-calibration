71d2c0e86b589d8d57e523e9a0a6f7962e0ee820
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-09-15 08:51:46 +0100

    Adding get sesnors of type.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ let express = require('express');
 let app = express();
 
 const sensorTypes = require('./lib/get-sensor-types.js')
+const sensorsOfType = require('./lib/get-sensors-of-type.js')
 const oracledb = require('oracledb')
 const dbconfig = require('./lib/db-config.js')
 
@@ -34,11 +35,39 @@ async function getSensorTypes() {
     })
 }
 
+async function getSensorsOfType(sensorType) {
+    return new Promise(async function(resolve, reject) {
+        try {
+            let sensors = await sensorsOfType.getSensorOfType(sensorType)
+            resolve(JSON.stringify(sensors))
+        }
+        catch(error) {
+            console.log(error)
+            reject(error)
+        }
+    })
+}
+
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/api/sensor-types", function (request, response) { 
     getSensorTypes()
+        .then((results, error) => {
+            if(error) {
+                response.writeHead(500, {'Content-Type': 'application/json'});    
+            }
+            else {
+                response.writeHead(200, {'Content-Type': 'application/json'});
+                response.end(results)
+            }
+        })
+});
+
+app.get("/api/sensor-types", function (request, response) { 
+    // get the sensor type from the query string
+    const sensorType = 'TZ' //  default to TZ for now
+    getSensorsOfType(sensorType)
         .then((results, error) => {
             if(error) {
                 response.writeHead(500, {'Content-Type': 'application/json'});    
