697113bbd7811123992d32b1d2f63677fd836953
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-09-15 08:52:33 +0100

    Adding onChange handler for sensor type select.

--- public/client.js
+++ public/client.js
@@ -1,10 +1,14 @@
 
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[name="sensor-type"]').onchange=sensorTypeChangeEventHandler;
+},false);
+
 (function() {
     // load the options for the sensor type select
     fetch('/api/sensor-types').then((response) => {
         return response.json()
     }).then((json) => {
-            let selectElement = document.getElementById("sensorTypeSelect")
+            let selectElement = document.getElementById("sensor-type")
             json.rows.forEach(sensorType => {
                 let newOption = document.createElement("option")
                 newOption.setAttribute("value", `${sensorType[0]}   ${sensorType[1]}`)
@@ -13,3 +17,9 @@
             })        
         })
 })();
+
+function sensorTypeChangeEventHandler(event) {
+
+    // load the sensors for the selected sensor type
+    
+}
